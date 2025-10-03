3a4618923702349d284317eb397043b9c4c5f8da
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2025-10-03 13:14:54 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,21 +1,20 @@
-# pip install ultralytics
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")  # alternatives: yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
+model = YOLO("yolov8n.pt")
 
 
-# Train the model
+# model specification for training
 results = model.train(
-    data="QR_Dataset/data.yml",  # path to your dataset YAML
-    epochs=50,                    # adjust depending on dataset size
-    imgsz=640,                    # resize images to 640x640
-    batch=16,                     # adjust according to your GPU
-    name="qr_yolo_model",         # folder name to save results
-    project="src/models",         # output folder for training runs
-    augment=True,                 # enable augmentations like flip, mosaic, mixup, HSV
-    optimizer='Adam',             # optional, Adam optimizer
-    lr0=0.01,                    # initial learning rate (adjust if needed)
+    data="QR_Dataset/data.yml", 
+    epochs=50,
+    imgsz=640,
+    batch=16, 
+    name="qr_yolo_model",        
+    project="src/models",       
+    augment=True,             
+    optimizer='Adam',        
+    lr0=0.01,
 )
 
-print("✅ Training finished! Model weights saved in:")
-print("src/model/qr_yolo_model/weights/")
+print("✅ Training finished! Model weights saved in src/models")
+
